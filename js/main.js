3572999494441a99cc2ac31c59ea3d3a1a97d501
pavel-035 <87231948+pavel-035@users.noreply.github.com>
2021-10-20 14:35:38 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,118 +13,183 @@ $(window).on('scroll', function() {
 	})
 });
 
-gsap.to(".header__human", {
-  scrollTrigger: {
-      trigger: ".header",
-        scrub: 1,
-        start: "top top",
-        end: "bottom center",
-        // markers: true
-  },
-  y: () => {
-  	return -100 * 100 / 1920 + 'vw';
-  }
-});
-gsap.to(".header__mountainBg", {
-  scrollTrigger: {
-      trigger: ".header",
-        scrub: 1,
-        start: "start 80%",
-        end: "bottom"
-  },
-  y: () => {
-  	return -500 * 100 / 1920 + 'vw';
-  }
-});
-gsap.to(".header__middleBlock", {
-	scrollTrigger: {
-	  	trigger: ".header",
-	    scrub: 1,
-	    start: "start 80%",
-	    end: "bottom"
-	},
-	y: () => {
-		return 600 * 100 / 1920 + 'vw';
+function installMediaQueryWatcher(mediaQuery, layoutChangedCallback) {
+	var mql = window.matchMedia(mediaQuery);
+	mql.addListener(function (e) { return layoutChangedCallback(e.matches); });
+	layoutChangedCallback(mql.matches);
+}
+
+installMediaQueryWatcher("(min-width: 600px)", function(matches) {
+	if (matches) {
+		gsap.to(".header__human", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "top top",
+				end: "bottom center",
+				// markers: true
+			},
+			y: () => {
+				return -100 * 100 / 1920 + 'vw';
+			}
+		});
+		gsap.to(".header__mountainBg", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+			},
+			y: () => {
+				return -500 * 100 / 1920 + 'vw';
+			}
+		});
+		gsap.to(".header__middleBlock", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+			},
+			y: () => {
+				return 600 * 100 / 1920 + 'vw';
+			}
+		});
+		gsap.to(".header", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+			},
+			backgroundPosition: '50% 100%'
+		});
+
+		gsap.to(".header", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+				// markers: true,
+			},
+			backgroundPosition: '50% 100%'
+		});
+
+		gsap.to('.first .card__discription', {
+			scrollTrigger: {
+				trigger: '.content',
+				start: 'top 50%',
+				end: 'bottom 50%',
+				scrub: 1,
+				// markers: true
+			},
+			y: 0
+		});
+		gsap.to('.first .card__image', {
+			scrollTrigger: {
+				trigger: '.content',
+				start: 'top 50%',
+				end: 'bottom 50%',
+				scrub: 1,
+				// markers: true
+			},
+			y: 0
+		});
+		gsap.to('.second .card__discription', {
+			scrollTrigger: {
+				trigger: '.content',
+				start: 'top 50%',
+				end: 'bottom 50%',
+				scrub: 1,
+				// markers: true
+			},
+			y: 0
+		});
+		gsap.to('.second .card__image', {
+			scrollTrigger: {
+				trigger: '.content',
+				start: 'top 50%',
+				end: 'bottom 50%',
+				scrub: 1,
+				// markers: true
+			},
+			y: 0
+		});
+		gsap.to('.third .card__discription', {
+			scrollTrigger: {
+				trigger: '.content',
+				start: 'top 50%',
+				end: 'bottom 50%',
+				scrub: 1,
+				// markers: true
+			},
+			y: 0
+		});
+		gsap.to('.third .card__image', {
+			scrollTrigger: {
+				trigger: '.content',
+				start: 'top 50%',
+				end: 'bottom 50%',
+				scrub: 1,
+				// markers: true
+			},
+			y: 0
+		});
+	} else {
+		gsap.to(".header__human", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "top top",
+				end: "bottom center",
+				// markers: true
+			},
+			y: () => {
+				return -100 * 100 / 600 + 'vw';
+			}
+		});
+		gsap.to(".header__mountainBg", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+			},
+			y: () => {
+				return -600 * 100 / 600 + 'vw';
+			}
+		});
+		gsap.to(".header__middleBlock", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start top",
+				end: "bottom"
+			},
+			y: () => {
+				return 600 * 100 / 600 + 'vw';
+			}
+		});
+		gsap.to(".header", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+			},
+			backgroundPosition: '50% 100%'
+		});
+
+		gsap.to(".header", {
+			scrollTrigger: {
+				trigger: ".header",
+				scrub: 1,
+				start: "start 80%",
+				end: "bottom"
+				// markers: true,
+			},
+			backgroundPosition: '50% 100%'
+		});
 	}
 });
-gsap.to(".header", {
-	scrollTrigger: {
-		trigger: ".header",
-		scrub: 1,
-		start: "start 80%",
-		end: "bottom"
-	},
-	backgroundPosition: '50% 100%'
-});
-
-gsap.to(".header", {
-	scrollTrigger: {
-		trigger: ".header",
-		scrub: 1,
-		start: "start 80%",
-		end: "bottom"
-		// markers: true,
-	},
-	backgroundPosition: '50% 100%'
-});
-
-gsap.to('.first .card__discription', {
-	scrollTrigger: {
-	  	trigger: '.content',
-	  	start: 'top 50%',
-	  	end: 'bottom 50%',
-	    scrub: 1,
-	    // markers: true
-	},
-	y: 0
-});
-gsap.to('.first .card__image', {
-	scrollTrigger: {
-	  	trigger: '.content',
-	  	start: 'top 50%',
-	  	end: 'bottom 50%',
-	    scrub: 1,
-	    // markers: true
-	},
-	y: 0
-});
-gsap.to('.second .card__discription', {
-	scrollTrigger: {
-	  	trigger: '.content',
-	  	start: 'top 50%',
-	  	end: 'bottom 50%',
-	    scrub: 1,
-	    // markers: true
-	},
-	y: 0
-});
-gsap.to('.second .card__image', {
-	scrollTrigger: {
-	  	trigger: '.content',
-	  	start: 'top 50%',
-	  	end: 'bottom 50%',
-	    scrub: 1,
-	    // markers: true
-	},
-	y: 0
-});
-gsap.to('.third .card__discription', {
-	scrollTrigger: {
-	  	trigger: '.content',
-	  	start: 'top 50%',
-	  	end: 'bottom 50%',
-	    scrub: 1,
-	    // markers: true
-	},
-	y: 0
-});
-gsap.to('.third .card__image', {
-	scrollTrigger: {
-	  	trigger: '.content',
-	  	start: 'top 50%',
-	  	end: 'bottom 50%',
-	    scrub: 1,
-	    // markers: true
-	},
-	y: 0
-});	
